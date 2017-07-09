44484dc3147d00d3a0821ac336f06fc2347a6d66
shijin <shijin@peer1>
2017-07-09 14:34:07 +0800

    implement server as oauth client and user as resource server

--- movingdemo-users/src/main/java/com/shijin/learn/movingdemo/users/UserApplication.java
+++ movingdemo-users/src/main/java/com/shijin/learn/movingdemo/users/UserApplication.java
@@ -2,8 +2,12 @@ package com.shijin.learn.movingdemo.users;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
+import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
+import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 
 /**
  * Hello world!
@@ -12,10 +16,11 @@ import org.springframework.cloud.netflix.feign.EnableFeignClients;
 @EnableFeignClients
 @EnableDiscoveryClient
 @SpringBootApplication
-public class UserApplication 
-{
-    public static void main( String[] args )
-    {
-        SpringApplication.run(UserApplication.class, args);
-    }
+@EnableOAuth2Sso
+@EnableResourceServer
+public class UserApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(UserApplication.class, args);
+  }
 }
